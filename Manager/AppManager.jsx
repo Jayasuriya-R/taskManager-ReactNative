f6855d59ba564a26eab15938e98d675ee160104d
Jayasuriya R <suryaabi3801@gmail.com>
2025-12-16 21:42:45 +0530

    updated folder structure

---
+++ Manager/AppManager.jsx
@@ -0,0 +1,20 @@
+import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import HomeScreen from '../Screen/HomeScreen'
+import AddTaskScreen from '../Screen/AddTaskScreen'
+import { Button } from 'react-native-paper'
+
+const AppManager = () => {
+    const [screen, setScreen] = useState(true)
+  return (
+    <View>
+      {
+        screen ? <HomeScreen/> : <AddTaskScreen/>
+      }
+
+      <Button  onPress={()=> setScreen(!screen)}>{screen ? "Add Task" : "Home"}</Button>
+    </View>
+  )
+}
+
+export default AppManager
